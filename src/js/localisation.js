f3cb6822c8293dad643e3c80ba7f5e4373a1ed1f
AkekoChan <artapp.theo@gmail.com>
2023-03-22 22:45:47 +0100

    final feature map

--- src/js/localisation.js
+++ src/js/localisation.js
@@ -33,30 +33,53 @@
         13
       );
 
-      let customIconMarket = L.icon({
-        iconUrl: `https://api.geoapify.com/v1/icon/?type=material&color=%23f25757&icon=store&apiKey=${App._API_KEY}`,
-        shadowUrl: `https://api.geoapify.com/v1/icon/?type=material&color=%23f25757&icon=store&apiKey=${App._API_KEY}`,
+      const customIconMarket = L.icon({
+        iconUrl: `https://api.geoapify.com/v1/icon/?type=material&color=%234fb477&icon=store&apiKey=${App._API_KEY}`,
+        iconSize: [31, 46],
+        iconAnchor: [15.5, 42],
+        popupAnchor: [0, -45],
       });
 
-      let currentLocation = L.marker([
-        `${crd.latitude}`,
-        `${crd.longitude}`,
-      ]).addTo(map);
-      currentLocation.bindPopup("<b>Votre position actuelle !").openPopup();
+      const customIconCurrentLoc = L.icon({
+        iconUrl: `https://api.geoapify.com/v1/icon/?type=material&color=%23f25757&icon=person&apiKey=${App._API_KEY}`,
+        iconSize: [31, 46],
+        iconAnchor: [15.5, 42],
+        popupAnchor: [0, -45],
+      });
+
+      let popup = L.popup({
+        className: "custom-popup",
+      }).setContent(`<p >Votre position actuelle !</p>`);
+
+      L.marker([`${crd.latitude}`, `${crd.longitude}`], {
+        icon: customIconCurrentLoc,
+      })
+        .addTo(map)
+        .bindPopup(popup)
+        .openPopup();
 
       const getMarket = async () => {
         try {
           const response = await fetch(
-            `https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=circle:${crd.longitude},${crd.latitude},1000&apiKey=${App._API_KEY}`
+            `https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=circle:${crd.longitude},${crd.latitude},5000&apiKey=${App._API_KEY}`
           );
           const markets = await response.json();
           console.log(markets);
           for (let market of markets.features) {
-            L.marker([
-              `${market.geometry.coordinates[1]}`,
-              `${market.geometry.coordinates[0]}`,
-              { icon: customIconMarket },
-            ]).addTo(map);
+            let popup = L.popup({
+              className: "custom-popup",
+            }).setContent(`<p >${market.properties.formatted}</p>`);
+
+            L.marker(
+              [
+                `${market.geometry.coordinates[1]}`,
+                `${market.geometry.coordinates[0]}`,
+              ],
+              { icon: customIconMarket }
+            )
+              .addTo(map)
+              .bindPopup(popup)
+              .on("click", clickZoom);
           }
         } catch (error) {
           console.error(error);
@@ -65,23 +88,19 @@
 
       getMarket();
 
-      let popup = L.popup();
-
-      const onMapClick = (e) => {
-        popup
-          .setLatLng(e.latlng)
-          .setContent("You clicked the map at " + e.latlng.toString())
-          .openOn(map);
-      };
-
-      map.on("click", onMapClick);
-
       L.tileLayer(
         `https://maps.geoapify.com/v1/tile/klokantech-basic/{z}/{x}/{y}.png?apiKey=${App._API_KEY}`,
         {
+          attribution:
+            'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
+          id: "klokantech-basic",
           maxZoom: 19,
         }
       ).addTo(map);
+
+      const clickZoom = (e) => {
+        map.setView(e.target.getLatLng());
+      };
     },
     /**
      * Renvoie une erreur si l'utilisateur n'a pas validé la géolocalisationS
