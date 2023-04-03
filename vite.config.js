ddc2f96346d10755411bd2f55ad898e6ec650243
unknown <sophie.garreta@gmail.com>
2023-04-03 20:52:38 +0200

    fix sw.js path and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,27 @@
 // vite.config.js
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import {resolve} from "path";
+import {defineConfig} from "vite";
 
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        newList: resolve(__dirname, "./pages/storage/new.html"),
-        editList: resolve(__dirname, "./pages/edit/edit.html"),
-        favShop: resolve(__dirname, "./pages/favShop/favoriteShop.html"),
-        parameters: resolve(__dirname, "./pages/parameters/parameters.html"),
-        shop: resolve(__dirname, "./pages/shop/shop.html"),
-        fallback: resolve(__dirname, "./pages/fallback/fallback.html"),
-        sw: resolve(__dirname, "sw.js"),
-      },
+    build: {
+        manifest: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                newList: resolve(__dirname, "./pages/storage/new.html"),
+                editList: resolve(__dirname, "./pages/edit/edit.html"),
+                favShop: resolve(
+                    __dirname,
+                    "./pages/favShop/favoriteShop.html"
+                ),
+                parameters: resolve(
+                    __dirname,
+                    "./pages/parameters/parameters.html"
+                ),
+                shop: resolve(__dirname, "./pages/shop/shop.html"),
+                fallback: resolve(__dirname, "./pages/fallback/fallback.html"),
+                sw: resolve(__dirname, "/sw.js"),
+            },
+        },
     },
-  },
 });
