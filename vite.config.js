c864e0b1f01e15cfd76fe885464a2ca1ace63594
AkekoChan <artapp.theo@gmail.com>
2023-04-04 21:49:14 +0200

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    manifest: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
