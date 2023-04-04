ee3965e738b6100d6be7577402416e4ad93f2749
AkekoChan <artapp.theo@gmail.com>
2023-04-04 22:33:42 +0200

    pwa change

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    publicDir: "public",
-    manifest: true,
+    // manifest: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
