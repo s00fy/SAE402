800c22a27fe957e4587605bf7689f64c93aab905
AkekoChan <artapp.theo@gmail.com>
2023-04-05 09:25:54 +0200

    links change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    manifest: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
