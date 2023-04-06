ebb1a833beaa1d974a1195e3dc56b3ea3c454320
AkekoChan <artapp.theo@gmail.com>
2023-04-06 22:15:31 +0200

    clipboard finish

--- src/js/autorisations.js
+++ src/js/autorisations.js
@@ -3,9 +3,10 @@
     // les variables/constantes
     _buttonNotif: document.querySelector(".request_notifications"),
     _buttonGeo: document.querySelector(".request_geolocation"),
+    _buttonClipboard: document.querySelector(".request_clipboard"),
 
     // allow to send notifs according to the checkbox state
-    _notifStatus: true,
+    _notifStatus: false,
 
     // initialisations
     app_init: function () {
@@ -19,6 +20,13 @@
       }
       if (App._buttonGeo) {
         App._buttonGeo.addEventListener("change", App.permissionGeo);
+      }
+
+      if (App._buttonClipboard) {
+        App._buttonClipboard.addEventListener(
+          "change",
+          App.permissionClipboard
+        );
       }
     },
 
@@ -59,7 +67,69 @@
      * Demande la permission pour activer la géolocalisation
      */
     permissionGeo: (e) => {
-      console.log(App._notifStatus);
+      if (e.target.checked) {
+        // Si l'utilisateur accepte la géolocalisation
+        if ("geolocation" in navigator) {
+          navigator.permissions
+            .query({ name: "geolocation" })
+            .then((permissionStatus) => {
+              console.log(permissionStatus);
+              if (permissionStatus.state === "granted") {
+                // Accès à la géolocalisation déjà autorisé
+                if (App._notifStatus === true) {
+                  const notif = new Notification(
+                    "Vous avez autorisé la géolocalisation !"
+                  );
+                  navigator.geolocation.getCurrentPosition(function (location) {
+                    newLocation(location);
+                  });
+                }
+              } else if (permissionStatus.state === "denied") {
+                // L'utilisateur a refusé la géolocalisation
+                App._buttonGeo.checked = false;
+              } else if (permissionStatus.state === "prompt") {
+                // Demande d'autorisation en attente
+                console.log("en attente...");
+
+                navigator.geolocation.getCurrentPosition(function (location) {
+                  newLocation(location);
+                });
+
+                return new Promise((resolve) => {
+                  permissionStatus.onchange = () => {
+                    resolve(permissionStatus.state);
+                  };
+                });
+              }
+            })
+            .then((result) => {
+              if (result === "granted") {
+                App._buttonGeo.checked = true;
+                if (newLocation) {
+                  navigator.geolocation.getCurrentPosition(function (location) {
+                    newLocation(location);
+                  });
+                }
+              } else if (result === "denied") {
+                App._buttonGeo.checked = false;
+              }
+            })
+            .catch((error) => {
+              console.error(error);
+              App._buttonGeo.checked = false;
+            });
+        } else {
+          console.log(
+            "La géolocalisation n'est pas supportée par ce navigateur."
+          );
+          App._buttonGeo.checked = false;
+        }
+      } else {
+        // Si l'utilisateur désactive la géolocalisation
+        console.log("L'utilisateur a désactivé la géolocalisation.");
+        App._buttonGeo.checked = false;
+      }
+
       function newLocation(location) {
         let userLocation = [];
         userLocation = {
@@ -68,55 +138,38 @@
         };
         console.log(userLocation);
       }
+    },
 
+    permissionClipboard: (e) => {
       if (e.target.checked) {
-        // If the user accepts send a notification
-        if ("geolocation" in navigator) {
-          if (navigator.permissions) {
-            navigator.permissions
-              .query({ name: "geolocation" })
-              .then((permissionStatus) => {
-                console.log(permissionStatus);
-                if (permissionStatus.state === "granted") {
-                  // Accès à la géolocalisation déjà autorisé
-                  if (App._notifStatus === true) {
-                    const notif = new Notification(
-                      "Vous avez autorisé la géolocalisation !"
-                    );
-                    navigator.geolocation.getCurrentPosition(function (location) {
-                      newLocation(location);
-                    });
-                  }
-                } else if (permissionStatus.state === "denied") {
-                  App._buttonGeo.checked = false;
-                } else if (permissionStatus.state === "prompt") {
-                  console.log("en attente...");
-
-                  navigator.geolocation.getCurrentPosition(function (location) {
-                    newLocation(location);
+        if (navigator.clipboard) {
+          navigator.permissions
+            .query({ name: "clipboard-write" })
+            .then((permissionStatus) => {
+              if (permissionStatus.state === "granted") {
+                navigator.clipboard
+                  .writeText("Hello World!")
+                  .then(() => {
+                    console.log("Le texte a été copié dans le presse-papier");
+                  })
+                  .catch((error) => {
+                    console.error("Impossible de copier le texte :", error);
                   });
-                  return new Promise((resolve) => {
-                    permissionStatus.onchange = () => {
-                      resolve(permissionStatus.state);
-                    };
-                  });
-                }
-              })
-              .then((result) => {
-                if (result === "granted") {
-                  App._buttonGeo.checked = true;
-                  if(newLocation){
-                    navigator.geolocation.getCurrentPosition(function (location) {
-                      newLocation(location);
-                    });
-                  }
-                } else if (result === "denied") {
-                  App._buttonGeo.checked = false;
-                }
-              });
-          }
+              } else if (permissionStatus.state === "prompt") {
+                console.warn("La demande de permission est en attente");
+              } else if (permissionStatus.state === "denied") {
+                console.error("La permission a été refusée");
+              }
+              permissionStatus.onchange = () => {
+                console.log(
+                  `Le statut de la permission a changé en ${permissionStatus.state}`
+                );
+              };
+            });
         } else {
-          console.log("Geolocation is not supported");
+          console.error(
+            "L'API Clipboard n'est pas disponible sur ce navigateur"
+          );
         }
       }
     },
